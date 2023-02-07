1d9b062d1b5ef701e7e0369fa0830ae92e8e593f
henryfeng <huili.f@gmail.com>
2023-02-07 21:31:15 +0800

    提交代码

---
+++ src/lib/Button.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+    import {createEventDispatcher} from "svelte";
+
+    export let secondary: boolean = false;
+    export let disabled: boolean = false;
+    export let size: 'big' | 'medium' | 'small' = 'medium';
+
+    export let style: string='';
+
+    const dispatch = createEventDispatcher();
+
+    let ignore = false;
+
+    const handleClickEvent = (e) => {
+        if (!disabled && !ignore) {
+            ignore = true;
+            dispatch('click', e);
+            setTimeout(()=>{
+                ignore = false;
+            }, 300);
+        }
+        e.stopPropagation();
+    }
+
+</script>
+<a class="button {size}" class:disabled={disabled} class:ignore={ignore} class:secondary={secondary} href="javascript:void(0)" {style} on:click={handleClickEvent}><slot/></a>
